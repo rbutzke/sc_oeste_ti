6298254b965e95963f5f7afb07757ad315f7cd73
rbutzke <roberto.butzke.junior@gmail.com>
2020-06-02 14:40:42 -0300

    metodos add , get

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const exphbs = require('express-handlebars')
 const app = express();
+const path = require('path');
 const db = require('./db/connection');
 const bodyParser = require('body-parser');
+const Job = require('./models/Job');
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
 
 const port = 3000;
 
@@ -13,6 +18,14 @@ app.listen(port, () => {
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// handle bars
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars');
+
+// static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
 // db connection
 db.authenticate().then(() => {
     console.log('Conectado com sucesso !!!');
@@ -22,7 +35,43 @@ db.authenticate().then(() => {
 
 // routes
 app.get('/', (req, res) => {
-    res.send('Api Express Ativa na porta:' + port);
+
+    let search = req.query.job;
+    let query = '%' + search + "%";
+
+    if (!search) {
+        Job.findAll({
+                order: [
+                    ['createdAt', 'DESC']
+                ]
+            })
+            .then(jobs => {
+                res.render('index', {
+                    jobs
+                });
+
+            })
+            .catch(err => console.log(err));
+    } else {
+        Job.findAll({
+                where: {
+                    title: {
+                        [Op.like]: query
+                    }
+                },
+                order: [
+                    ['createdAt', 'DESC']
+                ]
+            })
+            .then(jobs => {
+                res.render('index', {
+                    jobs,
+                    search
+                });
+
+            })
+            .catch(err => console.log(err));
+    }
 });
 
 // jobs routes
--- routes/jobs.js
+++ routes/jobs.js
@@ -7,6 +7,20 @@ router.get('/test', (req, res) => {
     res.send('Retorno do get OK').catch(err => console.log(err));
 });
 
+// detalhe da vaga
+router.get('/view/:id', (req, res) => Job.findOne({
+    where: { id: req.params.id }
+}).then(job => {
+    res.render('view', {
+        job
+    });
+}).catch(err => console.log(err)));
+
+
+router.get('/add', (req, res) => {
+    res.render('add');
+});
+
 // add job via post
 router.post('/add', (req, res) => {
     let { title, salary, company, description, email, new_job } = req.body;
